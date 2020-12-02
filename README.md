7a532a9a5da942c65300036eff5a5f043bcb80a2
Bálint Török <torokbalint2000@gmail.com>
2020-12-02 18:07:36 +0100

    adding readme and dropdown menu

---
+++ README.md
@@ -0,0 +1,62 @@
+# README
+
+- [HU](#hun-neptune)
+- [EN](#en-neptune)
+
+## <p id="hun-neptune">Neptune - Egy téma motor a Neptun weboldalhoz</p>
+
+****
+Szerintem középiskola óta hallhatjuk a rém meséket a Neptun rendszeréről. Ez a repo nem azért jött létre, hogy ezt a témát kivesézzük, és nem ias arra, hogy aki rátalál, ellene uszítsa.
+
+A Neptune az a Neptun rendszerhez íródot téma motor. Ritkán hallani manapság ezt szókapcsolatot, egy kis magyarázatot fűznék hozzá: a téma motorok nem tesznek semmi kártékonyat, nem nézik a jelszavaidat, felhasználó nevedet, szimplán az adott weboldal kinézetét változtatják meg.
+****
+
+Neptune az egy User script, ami felfrissíti a Neptunod kinézetét! Akartál valaha My Little Pony-s Neptunt? Vagy talán a Mézga családdal szembe találni magad minden bejelentkezéskor? Velünk ezt mind megteheted!
+
+Próbálunk minél egyszerűbb és modurálisabb témákat írni, hogy bárki képes legyen saját, egyedi témákat létrehozni, így teremtve meg az otthonosság érzetét.
+
+Ám a Neptune nem csak ennyiből áll: Tökéletes társa a [Neptun PowerUp-nak!](https://github.com/solymosi/npu) A mellett, hogy témát vltoztathatsz pár kisebb javítást is eszközöl, ami miatt sokkal simább émény fogad a honlapon. Ilyen változtatás például a téma választó, ahol most már legördülő menüből választhatod ki témáidat.
+
+### Tervezett témáink és elkészült témáink
+
+- Hyperdimension Neptunia AKA Neptune (✔): a motorunk becenevét adó téma. Az első elkészült témánk.
+- Menhera-chan (🔨)
+- Yotsuba (🔨)
+- My Littel Pony (🚧)
+- Satania (🚧)
+- Windows XP (🚧)
+- Mézga Kriszta / Mézga Család (🚧)
+- Rózsaszín párduc (🚧)
+- Stallman (🚧)
+- Lain (🚧)
+- Taiga (🚧)
+- One Punch Man (🚧)
+- Tohru (🚧)
+
+### Ötletek, jövőbeli elképzelések
+
+- Bejelentkezési lap téma.
+- Beállítások
+- Más téma források
+- Világ uralom
+
+### Miért pont Neptune?
+
+Nagyjából 2019 okt. 17. körül az S-Oszkár nevű facebook csoportban megjelent az alábbi meme:  
+![neptune meme](repo_assets\neptune_meme.png)
+
+Ez alapján jött ismerősöm ötlete, hogy csináljuk meg a Neptun honlapját egy neptune-s fejléccel.  
+Én ezt az ötletet tovább fejlesztve elkezdtem egy tényleges témát írni, belevályni magamat a témába. Pár javítást kellet eszközölnöm, hogy gond nélkül tudjam alkalmazni a témákat, így született meg a motor alapja.
+
+Ezek után csak jöttek a téma ötletek.
+
+### Még is mire is képes?
+
+Itt van pár példa:  
+![neptune tema](repo_assets\neptune_theme.jpg)
+![yotsuba tema](repo_assets\yotsuba_theme.jpg)
+
+## <p id="en-neptune">Neptune - A theme engine for the Neptun webpage</p>
+
+****
+****
--- user.js
+++ user.js
@@ -19,9 +19,22 @@
 var $ = window.jQuery;
 var ChooseBase = window.dochangeSkin;
 var cssElement = document.createElement("link");
+var selector = document.createElement('select');
+
 cssElement.rel = "stylesheet";
 cssElement.type = "text/css";
 cssElement.href = "";
+
+selector.id = 'skinSelectorDropdown';
+selector.style.width = "100px";
+selector.style.visibility ='hidden'
+selector.classList = ['skinchooserimg', 'skinchooserimgcollapsed']
+selector.setAttribute("aria-hidden", "false");
+selector.onchange = function()
+{
+  javascript: SkinChoose(this.value);
+  return false;
+}
 
 var arrow_up = $("[src$=\"searchpanel_up.png\"]")[0];
 var arrow_down = $("[src$=\"searchpanel_down.png\"]")[0];
@@ -37,35 +50,109 @@ window.Sys.WebForms.PageRequestManager.prototype._updatePanel = function(a, b)
 {
   b = b.replace(/"\S+16_ghb_close\.png"/gm, `"${base_url + 'svg/16_ghb_close.svg'}" style="height: 16px;"`);
   b = b.replace(/"\S+16_ghb_refresh\.png"/gm, `"${base_url + 'svg/16_ghb_refresh.svg'}" style="height: 16px;"`)
-  console.log(b);
   updatePanelBase(a, b);
 }
 
 window.CountDown = start;
 
+function getCurrentTheme()
+{
+  if(window.localStorage.getItem('CustomSkin') !== 'null')
+  {
+    return 'Skin_Neptun_Custom_' + window.localStorage.getItem('CustomSkin');
+  }
+  else
+  {
+    var head = $('head')[0];
+    var theme = head.innerHTML.match(/(?<=App_Themes\/)Skin_Neptun_\S+(?=\/skin_neptun_\S+\.css)/)[0];
+    console.log(theme);
+    return theme;
+  }
+}
+
 function showHideThemeChooser() {
 
-	var col = $('.skinchooserimgcollapsed')
+  var col = $('.skinchooserimgcollapsed')
+  var selector = $('#skinSelectorDropdown');
 
-    if (col != null) {
+    if (col != null && col.length !== 0) {
         var cho = $('.skinchooserimg')
-        cho.width("20px");
+        cho.width("100px");
         cho.animate({ height: "0px" }, 300);
         cho.animate({ height: "20px" }, 150);
         col.toggleClass("skinchooserimgcollapsed skinchooserimgexpanded", 300);
+        selector[0].style.visibility ='visible';
     }
     else {
         $('.skinchooserimgexpanded').toggleClass("skinchooserimgexpanded skinchooserimgcollapsed", 300);
+        selector[0].style.visibility ='hidden';
     }
 
     state = !state;
-    SetAriaHiddenAttributeToSkinChooserImages(state);
+    selector.attr('aria-hidden', state);
+
+}
+
+window.dochangeSkin = function(href, skin)
+{
+
+  for(var option in selector.options)
+  {
+    if(option.value === skin)
+    {
+      option.selected = true;
+    }
+    else if(option.selected)
+    {
+      option.selected = false;
+    }
+  }
+
+	if(!((String)(skin)).startsWith("Skin_Neptun_Custom"))
+	{
+    window.localStorage.setItem('CustomSkin',null);
+    return ChooseBase(href, skin)
+  }
+
+  var ls = skin.split('_');
+  var skinName = ls[ls.length - 1];
+
+  window.localStorage.setItem('CustomSkin',skinName);
+
+  selectSkin(skinName)
+
+}
+
+function selectSkin(skinName) {
+  if (skinName === "Neptune") {
+    cssElement.href = base_url + "Neptune/main.css?v=1";
+    arrow_right.src = base_url + "Neptune/right_arrow.png";
+		if (arrow_up !== undefined) {
+			arrow_up.src = base_url + "Neptune/searchpanel_up.png";
+		}
+		if (arrow_down !== undefined) {
+			arrow_down.src = base_url + "Neptune/searchpanel_down.png"
+		}
+    for (var i = 0; i< crosses.length; i += 1) {
+      crosses[i].src = base_url + "svg/16_ghb_close.svg";
+      crosses[i].style = "height: 16px;";
+    }
+    for (var j = 0; j< refreshers.length; j += 1) {
+      refreshers[j].src = base_url + "svg/16_ghb_refresh.svg";
+      refreshers[j].style = "height: 16px;";
+    }
+  }
 }
 
 function init()
 {
 
-	console.trace();
+    //fixing the chooser function by replacing the element.
+    var chooserButton = $('#imgSkinChooser')[0];
+
+    var clone = chooserButton.cloneNode(true);
+    clone.onclick = showHideThemeChooser;
+    chooserButton.parentElement.replaceChild(clone, chooserButton);
 
     //Theme chooser fix
     window.ShowHideThemeChooser = showHideThemeChooser;
@@ -99,11 +186,39 @@ function init()
 	    //Custom themes
     var skinChooser = ( document.getElementsByClassName("skinchooserimgcollapsed") || document.getElementsByClassName("skinchooserimgexpanded") )[0];
 
-    skinChooser.children[0].appendChild(createButton({name: "Anime1"}));
+    var ch = skinChooser.children[0].children // = ; //.appendChild(createButton({name: "Neptune"}));
+
+    var reg = /Skin_Neptun_\S+(?='|")/g
+    var selectedTheme = getCurrentTheme();
+    console.log(selectedTheme);
+
+    for(var i = 0; i < ch.length; i++)
+    {
+      
+      var option = document.createElement('option');
+      var name = ch[i].onclick.toString().match(reg)[0];
+	    console.log(name.toLowerCase());
+
+      option.value = name;
+      option.innerHTML = name.split('_').slice(-1)[0];
+
+      if(selectedTheme.toLowerCase() === name.toLowerCase())
+      {
+        option.selected = true;
+      }
+
+      selector.appendChild(option);
+
+    }
+
+    selector.appendChild(createOption({name:'Neptune'}));
+
+    skinChooser.children[0].innerHTML = "";
+    skinChooser.children[0].appendChild(selector);
 
     var previewColl = document.getElementsByClassName("skinsmallimage")[0];
 
-    addPreviewsto({parent: previewColl, name:"Anime1", src:"https://i.imgur.com/2HVmKTd.png"});
+    addPreviewsto({parent: previewColl, name:"Neptune", src:"https://i.imgur.com/2HVmKTd.png"});
 
     //Footer addition
     var footer = $('.footer')[0];
@@ -116,65 +231,29 @@ function init()
 
 }
 
-window.dochangeSkin = function(href, skin)
+function createOption({name=''})
 {
-
-	if(!((String)(skin)).startsWith("Skin_Neptun_Anime"))
-	{
-        window.localStorage.setItem('CustomSkin',null);
-        return ChooseBase(href, skin)
-    }
-
-    var ls = skin.split('_');
-    var skinName = ls[ls.length - 1];
-
-    window.localStorage.setItem('CustomSkin',skinName);
-
-    selectSkin(skinName)
-
-}
-
-function selectSkin(skinName) {
-  if (skinName === "Anime1") {
-    cssElement.href = base_url + "Neptune/main.css?v=1";
-    arrow_right.src = base_url + "Neptune/right_arrow.png";
-		if (arrow_up !== undefined) {
-			arrow_up.src = base_url + "Neptune/searchpanel_up.png";
-		}
-		if (arrow_down !== undefined) {
-			arrow_down.src = base_url + "Neptune/searchpanel_down.png"
-		}
-    for (var i = 0; i< crosses.length; i += 1) {
-      crosses[i].src = base_url + "svg/16_ghb_close.svg";
-      crosses[i].style = "height: 16px;";
-    }
-    for (var j = 0; j< refreshers.length; j += 1) {
-      refreshers[j].src = base_url + "svg/16_ghb_refresh.svg";
-      refreshers[j].style = "height: 16px;";
-    }
-  }
-}
-
-function createButton({name='', alt=null})
-{
-    var neptune = document.createElement("input");
-    neptune.type = "image";
-    neptune.name = "btnskin" + name;
-    neptune.id = "btnskin" + name;
-    neptune.tabIndex = 7;
-    neptune.classList = ["skinchooserimg"];
-    neptune.onblur = function(event){};
-    neptune.onfocus = function(event){};
-    neptune.src = "App_Themes/New_Common_Images/skin_pink_new.png";
-    neptune.alt = alt || "Anime";
-    neptune.style = "height: 20px; margin-bottom: 5px; cursor: pointer; width: 20px;"
-    neptune.setAttribute("aria-hidden", "false");
-    neptune.onclick = function(event){
-        javascript: SkinChoose('Skin_Neptun_'+name);
+    var neptune = document.createElement('option');
+    //neptune.type = "image";
+    //neptune.name = "btnskin" + name;
+    //neptune.id = "btnskin" + name;
+    //neptune.tabIndex = 7;
+    //neptune.classList = ["skinchooserimg"];
+    //neptune.onblur = function(event){};
+    //neptune.onfocus = function(event){};
+    //neptune.src = "App_Themes/New_Common_Images/skin_pink_new.png";
+    //neptune.alt = alt || "Anime";
+    //neptune.style = "height: 20px; margin-bottom: 5px; cursor: pointer; width: 20px;"
+    //neptune.setAttribute("aria-hidden", "false");
+    /*neptune.onclick = function(event){
+        javascript: SkinChoose('Skin_Neptun_Custom_'+name);
         return false;
-    }
+    }*/
     neptune.onmouseover = window.showSkinPreview.bind(neptune, { data: {skinName: name}});
     neptune.onmouseout = window.hideSkinPreview.bind(neptune, { data: {skinName: name}});
+    neptune.value = 'Skin_Neptun_Custom_' + name;
+    neptune.selected = ('Skin_Neptun_Custom_' + name) === getCurrentTheme();
+    neptune.innerHTML = name;
     return neptune;
 }
 
@@ -205,7 +284,7 @@ function start() {
 
     if( window.localStorage.getItem('CustomSkin') !== 'null')
     {
-		selectSkin(window.localStorage.getItem('CustomSkin'));
+		  selectSkin(window.localStorage.getItem('CustomSkin'));
     }
 
 }
