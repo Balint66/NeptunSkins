5aad2307ec62d8444d7381a898e0a975936110ee
Bálint Török <torokbalint2000@gmail.com>
2020-12-18 12:26:31 +0100

    Menhera light theme test

--- neptune.user.js
+++ neptune.user.js
@@ -61,7 +61,7 @@ var crosses = $('[src$="16_ghb_close.png"]');
 var refreshers = $('[src$="16_ghb_refresh.png"]');
 
 const base_url = "https://gitcdn.link/repo/Balint66/NeptunSkins/master/";
-var skins = ["Neptune", "Yotsuba", "Menhera-dark"];
+const skins = ["Neptune", "Yotsuba", "Menhera-dark", "Menhera-light"];
 
 var commoncss = document.createElement("link"); 
 commoncss.rel = "stylesheet";
