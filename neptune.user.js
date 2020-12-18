f80f56056b75be3b5966d3013939bfd6d803b493
Bálint Török <torokbalint2000@gmail.com>
2020-12-18 12:16:06 +0100

    now with language selection!

--- neptune.user.js
+++ neptune.user.js
@@ -11,26 +11,48 @@
 // @include        https://host.sdakft.hu/*
 // @include        https://neptun.ejf.hu/ejfhw/*
 // @grant          none
-// @version        2.0
+// @version        2.0.1
 // @author         --
 // @description    11/24/2020, 3:09:00 PM
+// @downloadURL    https://raw.githubusercontent.com/Balint66/NeptunSkins/master/neptune.user.js
 // ==/UserScript==
 
-var $ = window.jQuery;
+const $ = window.jQuery;
 var ChooseBase = window.dochangeSkin;
 
-var css = document.createElement("link");
+const css = document.createElement("link");
 css.rel = "stylesheet";
 css.type = "text/css";
 css.href = "";
 
-var selector = document.createElement('select');
-selector.id = 'skinSelectorDropdown';
-selector.style.width = "100px";
-selector.onchange = function() {
+const skin_selector = document.createElement('select');
+skin_selector.id = 'skinSelectorDropdown';
+skin_selector.style.width = "100px";
+skin_selector.onchange = function() {
   javascript: SkinChoose(this.value);
   return false;
 }
+
+const language_selector = document.createElement('select');
+language_selector.id = 'languageSelectorDropdown';
+language_selector.style.width = "100px";
+language_selector.onchange = function() {
+  if(this.value == "HU")
+  {
+    dochangeLanguage('0','1038')
+  }
+  else if(this.value == "EN")
+  {
+    dochangeLanguage('1','1033')
+  }
+  else if(this.value == "DE")
+  {
+    dochangeLanguage('2','1031')
+  }
+  return false;
+}
+
+const help_link = $('#lnkHelp')[0];
 
 var arrow_up = $("[src$=\"searchpanel_up.png\"]")[0];
 var arrow_down = $("[src$=\"searchpanel_down.png\"]")[0];
@@ -68,6 +90,11 @@ function getCurrentTheme() {
   }
 }
 
+function getCurrentLang()
+{
+  return (help_link.href || 'help/hweb_de.pdf').match(/(?<=help\/hweb_)\S+(?=\.pdf)/)[0];
+}
+
 function showHideThemeChooser() {
 
   var col = $('.skinchooserimgcollapsed')
@@ -90,7 +117,7 @@ function showHideThemeChooser() {
 }
 
 window.dochangeSkin = function(href, skin) {
-  for (var option in selector.options) {
+  for (var option in skin_selector.options) {
     if (option.value === skin) {
       option.selected = true;
     } else if (option.selected) {
@@ -137,11 +164,12 @@ init() {
   var h = document.querySelector('head');
   h.appendChild(commoncss);
   // fixing the chooser function by replacing the element.
+  /* Now with the selection replacement thi si redundant.
   var chooserButton = $('#imgSkinChooser')[0];
 
   var clone = chooserButton.cloneNode(true);
   clone.onclick = showHideThemeChooser;
-  chooserButton.parentElement.replaceChild(clone, chooserButton);
+  chooserButton.parentElement.replaceChild(clone, chooserButton);*/
 
   // Theme chooser fix
   window.ShowHideThemeChooser = showHideThemeChooser;
@@ -196,14 +224,44 @@ init() {
     if (selectedTheme.toLowerCase() === name.toLowerCase()) {
       option.selected = true;
     }
-    selector.appendChild(option);
+    skin_selector.appendChild(option);
   }
 
   for (var skin in skins) {
-    selector.appendChild(createOption({name : skins[skin]}));
+    skin_selector.appendChild(createOption({name : skins[skin]}));
   }
+
+  var currentLang = getCurrentLang();
+
+  var hu = document.createElement('option');
+  hu.value = 'HU';
+  hu.innerHTML = 'Magyar';
+  if(currentLang == 'hu')
+  {
+    hu.selected = true;
+  }
+  var en = document.createElement('option');
+  en.value = 'EN';
+  en.innerHTML = 'English';
+  if(currentLang == 'en')
+  {
+    en.selected = true;
+  }
+  var de = document.createElement('option');
+  de.value = 'DE';
+  de.innerHTML = 'Deutsch';
+  if(currentLang == 'de')
+  {
+    de.selected = true;
+  }
+  
+  language_selector.appendChild(hu);
+  language_selector.appendChild(en);
+  language_selector.appendChild(de);
+
   document.getElementsByClassName("langskinpartially")[0].innerHTML = "";
-  document.getElementsByClassName("langskinpartially")[0].appendChild(selector);
+  document.getElementsByClassName("langskinpartially")[0].appendChild(language_selector);
+  document.getElementsByClassName("langskinpartially")[0].appendChild(skin_selector);
 
   // Footer addition
   var footer = $('.footer')[0];
@@ -214,13 +272,13 @@ init() {
 }
 
 function createOption({name = ''}) {
-  var neptune = document.createElement('option');
-  neptune.onmouseover = window.showSkinPreview.bind(neptune, {data : {skinName : name}});
-  neptune.onmouseout = window.hideSkinPreview.bind(neptune, {data : {skinName : name}});
-  neptune.value = 'Skin_Neptun_Custom_' + name;
-  neptune.selected = ('Skin_Neptun_Custom_' + name) === getCurrentTheme();
-  neptune.innerHTML = name;
-  return neptune;
+  var skin_option = document.createElement('option');
+  skin_option.onmouseover = window.showSkinPreview.bind(skin_option, {data : {skinName : name}});
+  skin_option.onmouseout = window.hideSkinPreview.bind(skin_option, {data : {skinName : name}});
+  skin_option.value = 'Skin_Neptun_Custom_' + name;
+  skin_option.selected = ('Skin_Neptun_Custom_' + name) === getCurrentTheme();
+  skin_option.innerHTML = name;
+  return skin_option;
 }
 
 function
