edc972b1c7d6872d1e0ab193fc7b54a5787591b8
boa <boa@pm.me>
2021-02-17 23:35:52 +0100

    big PP

--- neptune.user.js
+++ neptune.user.js
@@ -56,7 +56,7 @@ var crosses = $('[src$="16_ghb_close.png"]');
 var refreshers = $('[src$="16_ghb_refresh.png"]');
 
 const base_url = "https://gitcdn.link/repo/Balint66/NeptunSkins/master/";
-const skins = ["Neptune", "Yotsuba", "Menhera-dark", "Menhera-light"];
+const skins = ["Neptune", "Yotsuba", "Menhera-dark", "Menhera-light", "PinkPanther"];
 
 var commoncss = document.createElement("link");
 commoncss.rel = "stylesheet";
